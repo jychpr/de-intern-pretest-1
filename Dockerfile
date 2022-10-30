8a496caa42e2449d6e650aa748857c55c1e33f3e
jychpr <joychrissetyoprajogo@gmail.com>
2022-10-30 10:23:24 +0700

    try to finalize

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.10
+
+LABEL version="0.1"
+LABEL maintainer="jychpr - Joy Chrissetyo Prajogo"
+LABEL release-date="2022-10-30"
+
+WORKDIR /workstation
+
+COPY final_requirements.txt final_requirements.txt
+COPY dwh-coding-challenge ./
+
+RUN pip install -r final_requirements.txt
+
+CMD [ "python", "./dwh-coding-challenge/solution/complete_answer.py"]
--- dwh-coding-challenge/solution/complete_answer.py
+++ dwh-coding-challenge/solution/complete_answer.py
@@ -2,7 +2,7 @@ import os
 import json
 import glob
 import pandas as pd
-from functools import reduce
+import sys
 
 # DECLARE PATH
 data_path_accounts = '../data/accounts/'
@@ -29,16 +29,30 @@ def process_json_data(filepath, dataframe, temp_df):
 
     return dataframe
 
+print("ANSWER NO.1")
+print("ACCOUNTS TABLE")
 table_accounts = process_json_data(data_path_accounts, dataframe_accounts, temp_df_accounts)
+print(table_accounts)
+print("\n")
+print("CARDS TABLE")
 table_cards = process_json_data(data_path_cards, dataframe_cards, temp_df_cards)
+print(table_cards)
+print("\n")
+print("SAVINGS ACCOUNTS TABLE")
 table_savings_accounts = process_json_data(data_path_savings_accounts, dataframe_savings_accounts, temp_df_savings_accounts)
+print(table_savings_accounts)
 
+print("\n")
+print("ANSWER NO.2")
+print("JOINED TABLES")
 join_step_1 = pd.merge(table_cards, table_savings_accounts, on='ts', how='outer')
 join_step_2 = pd.merge(table_accounts, join_step_1, on='ts', how='outer')
+print(join_step_2)
 
+print("\n")
+print("ANSWER NO.3")
 print("TRANSACTION LIST:")
 print(join_step_2.iloc[[24,25,26,29,30,32,34]])
 print("ANALYSIS:")
 print("TRANSACTION OCCURED 7 TIMES. CREDIT USED 3 TIMES: 12000 AND 19000 ON C1, 37000 ON C2. CHANGE OF BALANCE 4 TIMES: 21000, 15000, 40000, 33000.")
-
 
